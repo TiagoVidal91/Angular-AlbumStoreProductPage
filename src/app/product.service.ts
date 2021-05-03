e4ee3b1ce6d856ae878a9ad0361ae6a9f7b7050f
RAPOSO VIDAL Tiago Miguel <tiagomiguel.raposovidal@altran.com>
2021-05-03 16:00:50 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operator/map';
+import rxjs/add/operator/map;
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+_albumUrl: String="../assets/album.json"
+
+  constructor(private http: Http) { }
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl).response.json();
+  }
 
 }
