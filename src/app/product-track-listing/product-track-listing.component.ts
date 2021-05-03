657025eecb89baa0d27ba87bed54b79d9a194b24
RAPOSO VIDAL Tiago Miguel <tiagomiguel.raposovidal@altran.com>
2021-05-03 18:33:05 +0100

    Completed module

---
+++ src/app/product-track-listing/product-track-listing.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'app/product.service';
+import {Album} from '../album'
+
+@Component({
+  selector: 'app-track-listing',
+  templateUrl: './product-track-listing.component.html',
+  styleUrls: ['./product-track-listing.component.css']
+})
+export class TrackListingComponent implements OnInit {
+
+  albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+
+    return this._productService.getAlbum(1).subscribe(
+      response => this.albumInfo=response)
+
+  }
+
+}
